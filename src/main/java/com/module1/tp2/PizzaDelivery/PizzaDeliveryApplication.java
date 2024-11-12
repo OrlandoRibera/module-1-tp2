54f0b6b9451aa05b42e77c655f890599e038628c
orlando <orlando.riberas@gmail.com>
2024-11-11 21:56:13 -0400

    chore: init project

---
+++ src/main/java/com/module1/tp2/PizzaDelivery/PizzaDeliveryApplication.java
@@ -0,0 +1,12 @@
+package com.module1.tp2.PizzaDelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaDeliveryApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PizzaDeliveryApplication.class, args);
+  }
+}
